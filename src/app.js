5529e87684cb461e1ef6a85ce5a6b627ded3acc5
Vincent BERNIER <vincent.bernier@hotmail.fr>
2022-12-11 23:18:26 +0100

    carry on menu + made 1st carousel

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+
+//********** carousel 2 - swiper ***** */
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
