307c4d5d8e76efe4792b8d5d98d64b9fe500c21f
Vincent BERNIER <vincent.bernier@hotmail.fr>
2022-12-15 17:13:36 +0100

    desktop view Ok - responsive in process

---
+++ price-list.md
@@ -0,0 +1,19 @@
+# Pose faux ongle et entretiens
+
+- pose capsul gel   40
+- pose crablon  50
+- pose complète (avec semi-permanent)   65
+- suplément french   5
+- réparation ongle   5 à 10
+- dépose faux ongle   25
+- dépose + repose  65
+- strass    1/ongle  10
+  
+  # beauté des mains / pieds
+
+  - lissage 6
+  - pose vernis 5
+  - manucure complète avec cuticule 15
+  - vernis semi 25 
+  - dépose vernis + repose   40
+  - dépose vernis semi  10
--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
+//* ********* carousel 2 - swiper ***** */
 
+// eslint-disable-next-line no-undef, no-unused-vars
+const swiper = new Swiper('.mySwiper', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-//********** carousel 2 - swiper ***** */
-
-var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+})
