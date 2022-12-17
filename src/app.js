2be7d80509a0db69693c869a61d1fe41139f98ef
Vincent BERNIER <vincent.bernier@hotmail.fr>
2022-12-17 19:16:00 +0100

    bug fix and re deploy

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-//* ********* carousel 2 - swiper ***** */
-
-// eslint-disable-next-line no-undef, no-unused-vars
-const swiper = new Swiper('.mySwiper', {
+// eslint-disable-next-line no-undef
+new Swiper(".mySwiper", {
   cssMode: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
   mousewheel: true,
   keyboard: true,
-})
+});
